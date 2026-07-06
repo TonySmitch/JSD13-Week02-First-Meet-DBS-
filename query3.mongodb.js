b5c41b107eed565855a1801179ab32ad93a0cb23
Tony <tony@localhost.localdomain>
2026-07-06 16:46:01 +0700

    Updated Query

--- query3.mongodb.js
+++ query3.mongodb.js
@@ -1,3 +1,4 @@
+use("sample_mflix")
 db.theaters.find({ "location.address.state": "AL" }).count()
 db.theaters.find({ "location.address.city": "La Quinta" }).count()
 db.theaters.findOne({ "location.address.city": "La Quinta" })
